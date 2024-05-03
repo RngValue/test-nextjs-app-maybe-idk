2da80cb578e270d1d43ef1e2b27a09a52684bf80
RngValue <104089072+RngValue@users.noreply.github.com>
2024-05-03 18:53:15 +0200

    :3

---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from "@/components/navbar"
+
+export default function Blog() {
+   return (
+      <>
+         <Navbar></Navbar>
+         <p className="text-center">:3 fsadf....</p>
+      </>
+   );
+}
